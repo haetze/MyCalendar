9443105380f7c6177959e44c1da8efcb1f0cdecb
haetze <richy.sting@gmail.com>
2015-09-17 18:02:10 -0700

    basic funcionality done

--- main.hs
+++ main.hs
@@ -38,6 +38,11 @@ main = do
 					let day = getDayFromMonthFromYear m (read n) (Just thisYear)
 					let xs = showEventsFromDay day
 					putStr $ concat xs
+				"showDayInMonthInYear":x:u:w:_ -> do
+					let p = getYearFromCalendar (read w) calendar 
+					let day = getDayFromMonthFromYear (read u) (read x) p
+					let xs = showEventsFromDay day
+					putStr $ concat xs
 				"addEventToday":xs -> do
 					let e = Event $ unwords xs
 					let Just y = addEventToYear e m d thisYear
@@ -50,6 +55,17 @@ main = do
 					let e = Event $ unwords xs
 					let Just y2 = addEventToYear e (read y) (read x) thisYear
 					putMyCalendar $ addYearToCalendar y2 calendar 
+				"addEventOnDayInMonthInYear":x:u:w:xs -> do
+					let e = Event $ unwords xs
+					let p = getYearFromCalendar (read w) calendar
+					case p of
+						Nothing -> do
+							let q = createYearWithInt (read w)
+							let Just y2 = addEventToYear e (read u) (read x) q
+							putMyCalendar $ addYearToCalendar y2 calendar
+						Just q -> do 
+							let Just y2 = addEventToYear e (read u) (read x) q
+							putMyCalendar $ addYearToCalendar y2 calendar
 				"removeEventToday":xs -> do 
 					let e = Event $ unwords xs
 					let y = removeEventFromYear e m d thisYear
@@ -62,6 +78,29 @@ main = do
 					let e = Event $ unwords xs
 					let y2 = removeEventFromYear e (read y) (read x) thisYear
 					putMyCalendar $ addYearToCalendar y2 calendar
+				"removeEventOnDayInMonthInyear":x:u:w:xs -> do	
+					let e = Event $ unwords xs
+					let p = getYearFromCalendar (read w) calendar
+					case p of 
+						Nothing -> return ()
+						Just q  -> do
+							let y2 = removeEventFromYear e (read u) (read x) q
+							putMyCalendar $ addYearToCalendar y2 calendar
+				_ -> do
+					putStrLn "unknown Command\nKnown Commands:"
+					putStrLn "\tshowDay"
+					putStrLn "\tshowMonth"
+					putStrLn "\tshowDayInMonth <Day #>"
+					putStrLn "\tshowDayInMonthInYear <Day #> <Month #> <Year #>"
+					putStrLn "\taddEventToday <Event title, can be more than one word>"
+					putStrLn "\taddEventOnDay <Day #> <Event title, can be more than one word>"
+					putStrLn "\taddEventOnDayInMonth <Day #> <Month #> <Event title, can be more than one word>"
+					putStrLn "\taddEventOnDayInMonthInYear <Day #> <Month #> <Event title, can be more than one word>"
+					putStrLn "\tremoveEventToday <Event title, can be more than one word>"
+					putStrLn "\tremoveEventOnDay <Day #> <Event title, can be more than one word>"
+					putStrLn "\tremoveEventOnDayInMonth <Day #> <Month #> <Event, title, can be more than one word>"
+					putStrLn "\tremoveEventOnDayInMonthInyear <Day #> <Month #> <Year #> <Event title, can be more than one word>"
+					putStrLn "\tBug report? email to richy.sting@gmail.com"
 
 
 
